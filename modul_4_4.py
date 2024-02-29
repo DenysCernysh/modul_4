7a3c73f28a73e8626de3d7ffc345ad75ed64c98f
DenysCernysh <149713523+DenysCernysh@users.noreply.github.com>
2024-02-29 16:30:35 +0200

    text

--- modul_4_4.py
+++ modul_4_4.py
@@ -1,9 +1,11 @@
 contacts = {}
-def parce_input (user_input):
-    cmd,*args = user_input.split()
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
-    return cmd, *args
-def add_contact(args,contacts):
+    return cmd, args
+
+def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
@@ -19,38 +21,25 @@ def show_phone(name):
     if name in contacts:
         print(contacts[name])
     else:
-        print("contact not found.")
+        print("Contact not found.")
 
 def show_all():
     for name, phone_number in contacts.items():
         print(f"{name}: {phone_number}")
 
-def parse_input(user_input):
-    parts = user_input.split()
-    command = parts[0].lower()
-    args = parts[1:]
-    return command, args
-
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
     while True:
-        command = input("Enter a comand:)".srtip().lower())
+        command = input("Enter a command: ").strip().lower()
 
-        if command ii["close", "exit"]:
-            print("good bye!")
+        if command in ["close", "exit"]:
+            print("Goodbye!")
             break
         elif command == "hello":
-            print("Howe can I halp you?")
+            print("How can I help you?")
         else:
             print("Invalid command.")
 
-
-if __name__ = "__main__":
-    main(
-
-
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
+if __name__ == "__main__":
+    main()
